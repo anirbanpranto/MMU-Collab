a1a5bd87110f383df7e86e1cacca07182f9f0ac7
Anirban Bala <anirbanpranto@gmail.com>
2020-07-18 01:06:32 +0800

    Version 2.0

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Version History
 
 * V1.0 (Made the basic routes now testing out db)
+* V2.0 (Made Schema for all dbs)
--- app.py
+++ app.py
@@ -7,19 +7,69 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class student(db.Model):
+class Student(db.Model):
     student_id = db.Column(db.String(80), primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(80), nullable=False)
+    enrollments = db.relationship('Enrollment', backref = "enroller", lazy=True)
+    personal_tasks = db.relationship('Personal_task', backref="taskmaster", lazy=True)
+    reminders = db.relationship('Reminder', backref="worrier", lazy=True)
     def __repr__(self):
         return self
 
-class teacher(db.Model):
+class Enrollment(db.Model):
+    enroll_id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.String(80), db.ForeignKey('student.student_id'), nullable=False) #fk
+    course_id = db.Column(db.Integer, db.ForeignKey('course.course_id'), nullable=False) #fk
+    def __repr__(self):
+        return self
+
+class Personal_task(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.String(80), db.ForeignKey('student.student_id'), nullable=False) #fk
+    task_body = db.Column(db.String(240), nullable=False)
+    def __repr__(self):
+        return self
+
+class Post(db.Model):
+    post_id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.course_id'),nullable=False) #fk
+    post_body = db.Column(db.String(80), nullable=False)
+    def __repr__(self):
+        return self
+
+class Uni_task(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.course_id'),nullable=False) #fk
+    task_body = db.Column(db.String(100), nullable=False)
+    task_date = db.Column(db.String(80), nullable=False)
+    def __repr__(self):
+        return self
+
+class Course(db.Model):
+    course_id = db.Column(db.Integer, primary_key=True)
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.teacher_id') nullable=False) #fk
+    course_title = db.Column(db.String(80), nullable=False)
+    enrollments = db.relationship('Enrollment', backref = "enrolled", lazy=True)
+    posts = db.relationship('Post', backref = "classroom", lazy=True)
+    task_id = db.relationship('Uni_task', backref="coursework", lazy=True)
+    def __repr__(self):
+        return self
+
+class Reminder(db.Model):
+    reminder_id = db.Column(db.String(80), primary_key=True)
+    student_id = db.Column(db.String(80), db.ForeignKey('student.student_id'), nullable=False) #fk
+    reminder_date = db.Column(db.String(80), nullable=False)
+    def __repr__(self):
+        return self
+
+class Teacher(db.Model):
     teacher_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(80), nullable=False)
     password = db.Column(db.String(80), nullable=False)
+    courses = db.relationship('Course', backref='lecturer', lazy=True)
     def __repr__(self):
         return self
 ## Routes
